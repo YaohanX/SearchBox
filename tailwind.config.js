eb869d6696ec7eaa9f475ae511fb4ec77d70d245
Yaohan Xu <yaohxu@utu.fi>
2023-11-01 22:02:36 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/*
+ * @Author: Yaohan Xu yaohxu@utu.fi
+ * @Date: 2023-10-29 21:41:17
+ * @LastEditors: Yaohan Xu yaohxu@utu.fi
+ * @LastEditTime: 2023-10-31 22:25:19
+ * @FilePath: \Search Box\tailwind.config.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/index.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
